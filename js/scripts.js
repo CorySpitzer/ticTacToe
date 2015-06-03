181272dbf162115babe3f58be8521d42a6b7517b
Cory Spitzer & cleri philiastides <student@epicodus.com>
2015-06-03 14:25:58 -0700

    add winningCombo constructor

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,10 @@ var Space = function(coordinates, contains) {
   this.contains = contains;
 };
 
+var WinningCombo = function(space1, space2, space3) {
+  this.spaces = [space1, space2, space3];
+};
+
 var Board = function() {
   this.spaces = [ [new Space([0,0], ' '), new Space([0,1], ' '), new Space([0,2], ' ')],
                   [new Space([1,0], ' '), new Space([1,1], ' '), new Space([1,2], ' ')],
@@ -25,4 +29,22 @@ var Game = function(player1, player2) {
   this.player2 = player2;
   player2.game = this;
   this.board = new Board();
+  this.winner = 'none';
 };
+
+Game.prototype.gameOverCheck = function() {
+  this.winner = "person";
+  var blah = "";
+  this.board.spaces.forEach(function(column) {
+    if (column[0].contains === 'x') {
+      blah = "x";
+    } else {
+      blah = "y";
+    };
+  });
+  this.winner = blah;
+};
+
+// &&
+//      column[1].contains === 'x') &&
+//      column[2].contains === 'x')
--- spec/specs.js
+++ spec/specs.js
@@ -41,5 +41,45 @@ describe('Game', function() {
     var myBoard = new Board()
     expect(newGame.board).to.eql(myBoard);
     expect(clare.game).to.equal(newGame);
+    expect(newGame.winner).to.equal('none');
   });
 });
+
+describe('winningCombo', function() {
+  it('initializes a winning combo', function() {
+    var WinningComboA = new WinningCombo(new Space([0,0], ' '),
+                                          new Space([0,1], ' '),
+                                          new Space([0,2], ' ')
+    );
+    expect(WinningComboA.spaces).to.eql([new Space([0,0], ' '),
+                                          new Space([0,1], ' '),
+                                          new Space([0,2], ' ')])
+  });
+});
+
+  // it("determines a winner", function() {
+  //   var clare = new Player("Clare", "x");
+  //   var cory = new Player("Cory", "o");
+  //   var newGame = new Game(clare, cory);
+  //   cory.placeMark(1,1);
+  //   clare.placeMark(2,2);
+  //   cory.placeMark(0,2);
+  //   clare.placeMark(1,2);
+  //   cory.placeMark(2,0);
+  //   expect(newGame.winner()).to.equal(cory);
+  // });
+
+  // it("determines if a game is over because a player won", function() {
+  //   var clare = new Player("Clare", "x");
+  //   var cory = new Player("Cory", "o");
+  //   var newGame = new Game(clare, cory);
+  //   cory.placeMark(0,0);
+  //   clare.placeMark(1,1);
+  //   cory.placeMark(0,1);
+  //   // expect(newGame.gameOverCheck()).to.equal(false);
+  //   clare.placeMark(1,2);
+  //   cory.placeMark(0,2);
+  //   // expect(newGame.gameOverCheck()).to.equal(true);
+  //   newGame.gameOverCheck()
+  //   expect(newGame.winner).to.equal('x');
+  // });
